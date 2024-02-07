56411ef8aaed8a9783322802d1fdaefaa51beaa9
rossipedia <bryan.ross@docker.com>
2024-02-07 10:07:52 -0700

    example of MUI issue during SSR+Vite

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,47 @@
-import type { MetaFunction } from "@remix-run/node";
+import * as React from 'react';
+import type { MetaFunction } from '@remix-run/node';
+import { Link as RemixLink } from '@remix-run/react';
+import Typography from '@mui/material/Typography';
+import { ClientOnly } from 'remix-utils/client-only';
+import Link from '@mui/material/Link';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+// https://remix.run/docs/en/main/route/meta
+export const meta: MetaFunction = () => [
+  { title: 'Remix Starter' },
+  { name: 'description', content: 'Welcome to remix!' },
+];
 
+// https://remix.run/docs/en/main/file-conventions/routes#basic-routes
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <React.Fragment>
+      {/*
+      If uncommented then SSR blows up because this is an object with a
+      "default" property, as opposed to the actual default export of
+      `@mui/material/Typography`
+      */}
+
+      {/*
+      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+        Material UI Remix in TypeScript example
+      </Typography>
+      */}
+
+      {/*
+      However a client-only component works fine.
+      */}
+      <ClientOnly>
+        {() => (
+          <>
+            <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+              Material UI Remix in TypeScript example
+            </Typography>
+            <Link to="/about" color="secondary" component={RemixLink}>
+              Go to the about page
+            </Link>
+          </>
+        )}
+      </ClientOnly>
+    </React.Fragment>
   );
 }
